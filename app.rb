0a1a4e5a0c713daad2993457522120b573ca1757
grahammcbain@gmail.com <[grahammcbain@gmail.com]>
2014-06-26 16:25:45 -0600

    first sinatra app

--- app.rb
+++ app.rb
@@ -1,17 +1,29 @@
 require "sinatra/base"
-require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
+
 
   def initialize
     super
-    @items = []
+    @items = ["Channa Masala", "Chicken Tika Masala", "Saag Paneer"]
+
   end
 
   get "/" do
-    "items length: #{@items.length}"
+    erb :index, :locals => {:items => @items}
   end
+
+  get "/items" do
+  erb :items
+  end
+
+  get"/items/:identifier/" do
+    identifier = params[:identifier]
+    "You found item: #{identifier}"
+
+  end
+
+
 
   run! if app_file == $0
 end
